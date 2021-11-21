2538a1154ac08747a8491d3a2ce7adcfad66b0bc
zhoulizhao <894233613@qq.com>
2021-11-21 15:05:27 +0800

    第三周作业

---
+++ week03/src/main/java/com/homework/week03/inbound/HttpHandler.java
@@ -0,0 +1,84 @@
+package com.homework.week03.inbound;
+import com.homework.week03.client.OkHttpUtils;
+import com.homework.week03.filter.HeaderHttpRequestFilter;
+import com.homework.week03.filter.HttpRequestFilter;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.util.ReferenceCountUtil;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class HttpHandler extends ChannelInboundHandlerAdapter {
+    private HttpRequestFilter filter = new HeaderHttpRequestFilter();
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            System.out.println(uri);
+            //logger.info("接收到的请求url为{}", uri);
+            if (uri.contains("/test")) {
+                handlerTest(fullRequest, ctx, OkHttpUtils.getAsString("http://127.0.0.1:8081/"),filter);
+            } else {
+                handlerTest(fullRequest, ctx, "hello,others",filter);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx, String body, HttpRequestFilter filter) {
+        FullHttpResponse response = null;
+        try {
+            String value = body; // 对接上次作业的httpclient或者okhttp请求另一个url的响应数据
+            filter.filter(fullRequest,ctx);
+            System.out.println(fullRequest.headers().get("javax"));
+//            httpGet ...  http://localhost:8801
+//            返回的响应，"hello,nio";
+//            value = reponse....
+
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+
+        } catch (Exception e) {
+            System.out.println("处理出错:"+e.getMessage());
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+                ctx.flush();
+            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
---
+++ week03/src/test/java/com/homework/week03/Week02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.week03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
